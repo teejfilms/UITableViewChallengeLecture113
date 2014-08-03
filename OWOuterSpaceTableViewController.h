721bdf29073faf84f1b50fea40ad85fe293e0a75
TJ Gleason <TJ@TJs-MacBook-Pro.local>
2014-08-02 18:12:35 -0600

    first commit

--- OWOuterSpaceTableViewController.h
+++ OWOuterSpaceTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  OWViewController.h
+//  OWOuterSpaceTableViewController.h
 //  Out of this World
 //
 //  Created by TJ Gleason on 7/27/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OWViewController : UIViewController
+@interface OWOuterSpaceTableViewController : UITableViewController
 
 @end
